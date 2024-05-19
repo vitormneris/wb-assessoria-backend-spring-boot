b33f57053250000526d5eba83220b4ac7b677d42
DaniloYuri <>
2024-05-19 16:58:10 -0300

    vinculmar email com inserção de movimento implementado

--- src/main/java/com/br/wb/service/EmailService.java
+++ src/main/java/com/br/wb/service/EmailService.java
@@ -1,25 +1,31 @@
 package com.br.wb.service;
 
+import com.br.wb.domain.LastProcessMovement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import com.br.wb.dto.EmailDTO;
-
 @Service
-public class EmailService {
+public class EmailService extends AbstractMongoEventListener<LastProcessMovement> {
 
 	@Autowired
 	private JavaMailSender mailSender;
-	
-	public void sendEmail(EmailDTO email) {
+
+	@Override
+	public void onAfterSave(AfterSaveEvent<LastProcessMovement> event) {
+		LastProcessMovement objetoSalvo = event.getSource();
+		sendEmail(objetoSalvo);
+	}
+	public void sendEmail(LastProcessMovement lastProcessMovement) {
 		var message = new SimpleMailMessage();
 		
 		message.setFrom("joaomoreiraneris0@gmail.com");
-		message.setTo(email.to());
-		message.setSubject(email.subject());
-		message.setText(email.body());
+		message.setTo("welcomebrasil119@gmail.com");
+		message.setSubject("Movimentação no processo");
+		message.setText("Houve uma movimentação no seu processo!");
 		mailSender.send(message);
 	}
 }
